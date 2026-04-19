01c79404d9ef71b9dfa024032cbec92830969fa1
RafaPorcel <rafaelporcelllobregat@gmail.com>
2026-04-19 19:36:48 +0200

    refactor: renombrar paquete base a com.novaluz.tienda y actualizar pom.xml

--- backend-novaluz/src/main/java/com/novaluz/tienda/BackendNovaluzApplication.java
+++ backend-novaluz/src/main/java/com/novaluz/tienda/BackendNovaluzApplication.java
@@ -1,4 +1,4 @@
-package com.example.backendnovaluz;
+package com.novaluz.tienda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend-novaluz/src/test/java/com/novaluz/tienda/BackendNovaluzApplicationTests.java
+++ backend-novaluz/src/test/java/com/novaluz/tienda/BackendNovaluzApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.backendnovaluz;
+package com.novaluz.tienda;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
