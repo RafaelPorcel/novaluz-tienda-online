96ee1aafc73791decf2c55e31a91dbf75ae08e76
RafaPorcel <rafaelporcelllobregat@gmail.com>
2026-04-19 13:05:18 +0200

    feat: agregar proyecto Spring Boot 'backend-novaluz'

---
+++ backend-novaluz/src/main/java/com/example/backendnovaluz/BackendNovaluzApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendnovaluz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendNovaluzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendNovaluzApplication.class, args);
+    }
+
+}
---
+++ backend-novaluz/src/test/java/com/example/backendnovaluz/BackendNovaluzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendnovaluz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendNovaluzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
